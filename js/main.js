0bda4dbb5fd8e0fcd9227e3cee0c32acafea1181
armeniaquindiocontraentregaco <armeniaquindio@contraentregaco.com>
2024-11-14 18:16:18 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -62,8 +62,8 @@
 
     // Inicializa el asistente
 VapiChatbot.init({
-  assistantId: 'TU_ASISTENTE_ID',
-  apiKey: 'TU_API_KEY',
+  assistantId: '08974042-e8ba-4de7-b75f-4d2e26d72334',
+  apiKey: 'f0e792ec-1aa7-4b69-b592-0eee9026d836',
 });
 
 // Abre el asistente al hacer clic en el botón
